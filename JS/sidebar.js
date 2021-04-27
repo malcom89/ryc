35f0180907dac85aaa24195004dc6370ef7244aa
unknown <luismanuelandujarpichardo@gmail.com>
2021-04-26 22:23:09 -0400

    update login, sidebar, prestamo create

---
+++ JS/sidebar.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $('.nav-item > .drop').click(function (e) {
+        e.preventDefault();
+
+        
+        
+        
+
+
+        $('.nav-item > .uld').addClass('d-none');
+            $(this).next('.uld').removeClass('d-none');
+        
+        
+    })
+})
