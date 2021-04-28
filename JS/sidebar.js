a98a7e663d3f65e6ec3f697cf3d4658d4d9bf5cd
marcos andujar <marcos-andujar@hotmail.com>
2021-04-27 20:23:03 -0400

    hjg

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -6,36 +6,11 @@
         
         
       
-        if (!$(this).next('.uld').hasClass('d-none')) {
-
-
-        $('.nav-item > .uld').addClass('d-none');
-
-
-
-
-        } else {
+        
 
         $('.nav-item > .uld').addClass('d-none');
             $(this).next('.uld').removeClass('d-none');
-        }
-
         
         
-    })
-
-
-    $('#menu').click(function (e) {
-        e.preventDefault();
-
-        if ($('#sideba').hasClass('activ')) {
-
-            $('#sideba').removeClass('activ');
-            $('#content').removeClass('col-md-12');
-        } else {
-
-            $('#sideba').addClass('activ');
-            $('#content').addClass('col-md-12');
-        }
     })
 })
